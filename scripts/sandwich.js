ec2fec2a4bd8233f60d00c1ec035acb7a61ec973
kayshwii <kayshwii@icloud.com>
2022-09-11 23:51:03 -0500

    replaced spliced value with "air" to fix indexing issue

--- scripts/sandwich.js
+++ scripts/sandwich.js
@@ -1,8 +1,8 @@
 //store ingredient images
 const ingredientImgs = {
-    "bread" : "images/bread.png",
-    "cheese" : "images/cheese.png",
-    "ham" : "images/ham.png"
+    "Bread" : "images/bread.png",
+    "Cheese" : "images/cheese.png",
+    "Ham" : "images/ham.png"
 }
 
 //store history of ingredients in current sandwich
@@ -12,7 +12,6 @@ const ingredientList = []
 function addTopping(topping) {
     //add the topping to the ingredient list
     ingredientList.push(topping);
-    //
     localStorage.setItem('sandwich', JSON.stringify(ingredientList));
 
     for (let ingredient in ingredientList) {
@@ -25,6 +24,8 @@ function addTopping(topping) {
     }
     var plate = document.getElementById("plate");
     plate.append(slices);
+
+    //pass control to the history function
     addToppingHistory(topping);
 }
 
@@ -32,12 +33,10 @@ function addToppingHistory(topping) {
     for (let ingredient in ingredientList) {
         //for every topping add a button to remove the item
     var delButton = document.createElement("button");
-    var iTag = document.createElement("i");
     delButton.className = "delButton"
     delButton.value = ingredient;
     delButton.id = topping + ingredient++;
     delButton.innerHTML = topping + " &#10060;";
-    delButton.appendChild(iTag);
     }
     var history = document.getElementById("history");
     history.append(delButton);
@@ -48,7 +47,7 @@ document.addEventListener("click", removeTopping);
 function removeTopping (click) {
     var button = click.target;
     if (button.className == "delButton") {
-        ingredientList.splice(button.value, 1);
+        ingredientList.splice(button.value, 1, "air");
         document.getElementById(button.id).remove();
         document.getElementById(button.id).remove();
     }
